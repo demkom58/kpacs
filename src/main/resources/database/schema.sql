f78eae1c1f4df3ea920697a80db6637c9d07ee06
Max Demydenko <m_demy@pm.me>
2024-05-23 13:03:44 +0300

    feat: add indexes and test data

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,17 +1,21 @@
-CREATE TABLE IF NOT EXISTS  kpacs
+-- Language: MySQL
+
+CREATE TABLE IF NOT EXISTS kpacs
 (
     id            INT AUTO_INCREMENT PRIMARY KEY,
     title         VARCHAR(250) NOT NULL,
     description   VARCHAR(2000),
     creation_date VARCHAR(10)  NOT NULL,
-    UNIQUE (title)
+    UNIQUE (title),
+    INDEX (id)
 );
 
-CREATE TABLE IF NOT EXISTS  kpac_sets
+CREATE TABLE IF NOT EXISTS kpac_sets
 (
     id    INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(250) NOT NULL,
-    UNIQUE (title)
+    UNIQUE (title),
+    INDEX (id)
 );
 
 CREATE TABLE IF NOT EXISTS kpac_set_kpacs
@@ -20,5 +24,6 @@ CREATE TABLE IF NOT EXISTS kpac_set_kpacs
     kpac_id     INT,
     PRIMARY KEY (kpac_set_id, kpac_id),
     FOREIGN KEY (kpac_set_id) REFERENCES kpac_sets (id),
-    FOREIGN KEY (kpac_id) REFERENCES kpacs (id)
+    FOREIGN KEY (kpac_id) REFERENCES kpacs (id),
+    INDEX (kpac_set_id)
 );
